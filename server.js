52ebd69795efdfd9637296c458be17889c120b02
teachershriti1 <72126068+teachershriti1@users.noreply.github.com>
2023-12-23 11:07:14 +0530

    mail

--- server.js
+++ server.js
@@ -26,8 +26,8 @@ const transporter = nodemailer.createTransport({
     port: 465,
     host: "smtp.gmail.com",
     auth: {
-        user: '',
-        pass: '',
+        user: 'shritibadola@gmail.com',
+        pass: 'rnnpyallkdmctdzx',
     },
     secure: true,
 });
@@ -44,7 +44,7 @@ app.post("/send-mail", (req, res) => {
     const to = req.body.to;
     const url = req.body.url;
     const mailData = {
-        from: "apoorv.goyal@whitehatjr.com",
+        from: "shritibadola@gmail.com",
         to: to,
         subject: "Join the video chat with me!",
         html: `<p>Hey there,</p><p>Come and join me for a video chat here - ${url}</p>`
